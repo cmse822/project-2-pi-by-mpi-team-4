eaa7f77758ce8224f95657424b8e5f81f612febb
nicklow1 <nicklow1@msu.edu>
2024-02-10 12:58:46 -0500

    moving cout

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,14 @@
 
 using namespace std;
 
-cout << "Before initialize" << endl;
+
 
 int main(int argc, char* argv[]) 
 {
     int numtasks, rank;
+
+    cout << "Before initialize" << endl;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
